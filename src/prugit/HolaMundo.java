de9911a9dd0849f92d0e600b63a0a0cc5b2beffc
Vanegas9090 <mariocanario98@gmail.com>
2019-05-07 15:42:30 -0600

    Agregue un nuevo archivo

---
+++ src/prugit/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prugit;
+
+/**
+ *
+ * @author vanegas9090
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola hector que pedo");
+    }
+}
